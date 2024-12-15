c1d11fc032cf30956a2bfcb629bb596147fc44fe
o <o@dmstfctn.net>
2024-12-15 17:50:31 +0000

    dump script to auto convert sheets into .csv (for unreal) and .json (for leonardo)

---
+++ dump.js
@@ -0,0 +1,32 @@
+const fs = require('node:fs');
+const path = require('node:path');
+var csv = require("csvtojson");
+
+const {getSheets} = require('./modules/googleSheets.js');
+
+const tabs = [
+    'props',
+    'backdrops',
+    'characters',
+    'narrator'
+];
+
+(async function () {
+    const sheets = await getSheets( tabs );
+    console.log(sheets)
+    for( const name in sheets ){
+        const sheet = sheets[name];
+        const sheetAsCsv = await sheet.downloadAsCSV();
+        fs.writeFileSync( 
+            path.join( __dirname, 'csv', `${name}.csv` ), 
+            sheetAsCsv 
+        );
+        const sheetAsJson = await csv().fromString( sheetAsCsv.toString() );
+        console.log( sheetAsJson );
+        const out = (name === 'narrator') ? sheetAsJson[0] : sheetAsJson;
+        fs.writeFileSync( 
+            path.join( __dirname, 'json', `${name}.json` ), 
+            JSON.stringify( out, false, 2) 
+        );
+    }
+})();
